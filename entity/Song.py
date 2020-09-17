fb29cded87047f6abb28eb86147ee1d83184eb91
陈飞 <chenfei@beantechs.cn>
2020-09-17 16:15:05 +0800

    增加str输出

--- entity/Song.py
+++ entity/Song.py
@@ -18,3 +18,21 @@ class Song:
     @name.setter
     def name(self, value):
         self._name = value
+
+    def __str__(self):
+        return "[id=" + str(self._id) + \
+               ", name=" + self._name + "]"
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        if isinstance(other, Song):
+            return self._id == other._id and self._name == other._name
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(self._id) + hash(self._name)
